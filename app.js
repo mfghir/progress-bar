80648068610f36db6f6e48b06d669f00110b331d
miss-f-g <fatemeghafari98@gmail.com>
2022-06-29 13:03:32 +0430

    progress bar

---
+++ app.js
@@ -0,0 +1,22 @@
+const progressBar = document.querySelector("#progress-bar");
+const section = document.querySelector("section");
+
+const animationProgressBar = () => {
+  let scrollDistance = -section.getBoundingClientRect().top;
+//   console.log(scrollDistance);
+
+  let progressWidth =
+    (scrollDistance /
+      (section.getBoundingClientRect().height -
+        document.documentElement.clientHeight)) *
+    100;
+
+  let value = Math.floor(progressWidth);
+  progressBar.style.width = value + "%";
+
+  if (value < 0) {
+    progressBar.style.width = "0%";
+  }
+};
+
+window.addEventListener("scroll", animationProgressBar);
